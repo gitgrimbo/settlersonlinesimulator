a29a6894041c5ec6ee282fc431b84e55ed33076d
gitgrimbo <gitgrimbo@gmail.com>
2013-10-05 23:56:01 +0100

    Add jQuery nonsense for bookmarklets. Need to find a better way.

--- full-adventure-info-init.js
+++ full-adventure-info-init.js
@@ -1,3 +1,17 @@
+// jQuery dance
+// jQuery should be on the page already, but we want to use it as a module.
+(function() {
+	function maybeDefine(moduleName, scope, property) {
+		var ob = scope[property];
+		if (ob) {
+			define(moduleName, [], function() {
+				return ob;
+			});
+		}
+	}
+	maybeDefine("jquery", window, "jQuery");
+}());
+
 define(["./full-adventure-info"], function(fai) {
     fai.execute();
 });
