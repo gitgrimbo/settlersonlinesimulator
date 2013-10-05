75a9ce6e6cfa55ca3b1e5141edc87b74c878acaa
gitgrimbo <gitgrimbo@gmail.com>
2013-10-05 17:31:36 +0100

    Tidy SpecRunner.html.

---
+++ test/requirejs-config.js
@@ -0,0 +1,9 @@
+require = {
+    baseUrl: "../",
+    paths: {
+        "domReady": "https://rawgithub.com/requirejs/domReady/2.0.1/domReady",
+        "text": "https://rawgithub.com/requirejs/text/2.0.10/text",
+        "spec": "./test/spec",
+        "html": "./test/html"
+    }
+};
