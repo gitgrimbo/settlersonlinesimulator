fcbbc69caac05ad0af5248cb1706568be02851de
gitgrimbo <gitgrimbo@gmail.com>
2013-12-16 14:27:31 +0000

    Make sure all taskkills fire.

--- bin/kill-drivers.bat
+++ bin/kill-drivers.bat
@@ -1,3 +1,7 @@
+@echo off
 REM Run from anywhere, project root or bin folder. E.g. (from root):
 REM   bin\kill-drivers
-tasklist | find /i "driver" && taskkill /f /im "chromedriver.exe" && taskkill /f /im "IEDriverServer.exe"
+tasklist | findstr /i "driver phantomjs"
+taskkill /f /im "chromedriver.exe"
+taskkill /f /im "IEDriverServer.exe"
+taskkill /f /im "phantomjs.exe"
