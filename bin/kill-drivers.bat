c327971a20bba9d173514734e60a8721c0421b39
gitgrimbo <gitgrimbo@gmail.com>
2014-02-06 00:03:01 +0000

    Exit properly so that scripts can be chained ...
    e.g.:
    
      bin\kill-drivers.bat && bin\node-setup\node.bat

--- bin/kill-drivers.bat
+++ bin/kill-drivers.bat
@@ -1,7 +1,9 @@
 @echo off
 REM Run from anywhere, project root or bin folder. E.g. (from root):
 REM   bin\kill-drivers
-tasklist | findstr /i "driver phantomjs"
-taskkill /f /im "chromedriver.exe"
-taskkill /f /im "IEDriverServer.exe"
-taskkill /f /im "phantomjs.exe"
+call tasklist | findstr /i "driver phantomjs"
+call taskkill /f /im "chromedriver.exe"
+call taskkill /f /im "IEDriverServer.exe"
+call taskkill /f /im "phantomjs.exe"
+
+exit /b 0
