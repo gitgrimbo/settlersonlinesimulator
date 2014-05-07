af28f6008e13b6999c7fd5570a138a27eef30802
gitgrimbo <gitgrimbo@gmail.com>
2014-05-07 19:51:13 +0100

    Improve URL pattern matching for extension.

--- chrome/main.js
+++ chrome/main.js
@@ -17,21 +17,36 @@
 
 require(["adventures-page", "full-adventure-info", "sort-adventures", "units-required/ui-app"], function(adventuresPage, fai, sortAdventures, unitsRequired) {
     function match(action, location) {
-        var r = action.re.test(location[action.match]);
-        return r;
+        var tests = action.tests;
+        for (var i = 0; i < action.tests.length; i++) {
+            if (action.tests[i].re.test(location[action.tests[i].prop])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function Test(prop, re) {
+        this.prop = prop;
+        this.re = re;
     }
 
     // data structure to let us execute some code when a URL match is found
     var actions = [];
     actions.push({
-        match: "pathname",
-        re: new RegExp("^/dso_kampfsimulator/en/adventures/$"),
+        tests: [
+            new Test("href", /\/\/localhost\/.*\/adventures.html$/),
+            new Test("pathname", /\/dso_kampfsimulator\/en\/adventures\/$/)
+        ],
         execute: function() {
             fai.execute();
         }
     }, {
-        match: "pathname",
-        re: new RegExp("^/dso_kampfsimulator/en/adventures/.+$"),
+        tests: [
+            new Test("href", /\/\/localhost\/.*\/adventures\/.+$/),
+            new Test("pathname", /\/dso_kampfsimulator\/en\/adventures\/.+$/)
+        ],
         execute: function() {
             unitsRequired.execute();
         }
