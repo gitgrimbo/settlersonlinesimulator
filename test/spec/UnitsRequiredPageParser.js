9fe20727ca31acd60365689374aad66da0f9b8b5
gitgrimbo <gitgrimbo@gmail.com>
2014-09-21 09:33:21 +0100

    Test for a MMA (Master of Martial Arts) battle.

--- test/spec/UnitsRequiredPageParser.js
+++ test/spec/UnitsRequiredPageParser.js
@@ -1,12 +1,17 @@
 /*global:describe,beforeEach,it,expect,spyOn*/
-define(["units-required/model/UnitList", "units-required/PageParser", "text!html/adventures/der-schamane.html", "text!html/adventures/sattelfest.html"], function(UnitList, UnitsRequiredPageParser, derSchamaneHtml, sattelfestHtml) {
+define([
+    "units-required/model/UnitList",
+    "units-required/PageParser",
+    "text!html/adventures/der-schamane.html",
+    "text!html/adventures/sattelfest.html",
+    "text!html/adventures/verraeter-with-mma.html"
+], function(UnitList, UnitsRequiredPageParser, derSchamaneHtml, sattelfestHtml, verraeterWithMMAHtml) {
 
-    function enemy() {
-        var result = {};
-        for (var i = 0; i < arguments.length; i += 2) {
-            result[arguments[i]] = arguments[i + 1];
-        }
-        return result;
+    function enemy(count, type) {
+        return {
+            count: count,
+            type: type
+        };
     }
 
     describe("UnitsRequiredPageParser.getAttackPlanFromHtml", function() {
@@ -25,7 +30,7 @@ define(["units-required/model/UnitList", "units-required/PageParser", "text!html
             var sim0 = sims[0];
             expect(sim0).not.toBeUndefined();
 
-            expect(sim0.campEnemies).toEqual([enemy("count", "100", "type", "Scavenger (PL)"), enemy("count", "50", "type", "Ranger (WL)")]);
+            expect(sim0.campEnemies).toEqual([enemy("100", "Scavenger (PL)"), enemy("50", "Ranger (WL)")]);
         });
 
         it("parses sattelfest.html", function() {
@@ -38,7 +43,20 @@ define(["units-required/model/UnitList", "units-required/PageParser", "text!html
             var sim0 = sims[0];
             expect(sim0).not.toBeUndefined();
 
-            expect(sim0.campEnemies).toEqual([enemy("count", "70", "type", "Nomad (NO)")]);
+            expect(sim0.campEnemies).toEqual([enemy("70", "Nomad (NO)")]);
+        });
+
+        it("parses verraeter-with-mma.html", function() {
+            var attackPlan = UnitsRequiredPageParser.getAttackPlanFromHtml(verraeterWithMMAHtml);
+            expect(attackPlan).not.toBeUndefined();
+
+            var sims = attackPlan.sims;
+            expect(sims.length).toEqual(11);
+
+            var sim0 = sims[0];
+            expect(sim0).not.toBeUndefined();
+
+            expect(sim0.campEnemies).toEqual([enemy("40", "Militia Deserter (DM)"), enemy("60", "Longbow Deserter (DLB)")]);
         });
 
     });
